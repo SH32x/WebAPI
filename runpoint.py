911b58f6decb55cd41c9366c435e5b1d34027ec6
Soroush Heydari <soroush.heydari@ontariotechu.net>
2024-10-20 23:42:17 -0400

    Update
    Added launch config and DB config files, along with more app structure.

--- runpoint.py
+++ runpoint.py
@@ -1,6 +1,7 @@
 # Application runpoint file
 
 from flask import Flask
+from app import app
 app = Flask(__name__)
 
 
@@ -8,3 +9,10 @@ app = Flask(__name__)
 @app.route('/')
 def init_run():
     return 'Testing output...'
+
+@app.route('/page/<int:page_num>')
+def content(page_num):
+    return f'<h1>Welcome to page {page_num} of this webapp!</h1>'
+
+if __name__ == '__main__':
+    app.run()
